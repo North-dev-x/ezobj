bb6e966147433232087a948008f2c5e34186ab29
North <aadenpatrick@live.com>
2024-12-09 09:02:55 -0800

    Add documentation and module lua file

---
+++ Classes.md
@@ -0,0 +1,55 @@
+A class is created by calling the module as a function.
+```lua
+function ezobj:__call<T>(object: T): Object<T>
+```
+Creates a class with the given table as its members.
+`object` must be an [empty table](ezobj/Abstract%20Classes) or a dictionary(key-value pairs).
+
+Example usage:
+```lua
+local class = require(ezobj-path)
+
+local Foo = class {
+	bar = 50;
+	foobar = function(self: Foo)
+		print(self.bar)
+	end
+}
+type Foo = typeof(Foo)
+```
+
+### Instantiating Classes
+
+Classes can be instantiated with `Class.new()`.
+```lua
+local newFoo = Foo.new()
+newFoo:foobar() -- 50
+```
+
+Classes can also be instantiated with initial values.
+```lua
+local newFoo = Foo.new {
+	bar = 4905;
+}
+newFoo:foobar() -- 4905
+```
+
+Classes do not need to be instantiated to be used, but if not instantiating or extending, I would recommend creating a normal table with the same values.
+```lua
+Foo:foobar() -- 50
+```
+
+### Extending Classes
+
+Classes can be extended with `Class:extend {}`.
+```lua
+local Bar = Foo:extend {
+	foo = 30; -- new member unique to this class
+	bar = "bar"; -- overrides field bar from superclass
+}
+Bar:foobar() -- "bar" - inherits this method from superclass Foo
+Bar.super:foobar() -- 50 - superclass methods only access the super table
+```
+Doing this creates a new class, with all of the members of the superclass, and any added in the provided table.
+
+The superclass itself can be accessed with `Class.super`.
---
+++ ezobj.lua
@@ -0,0 +1,137 @@
+local ezobj = {}
+
+local function getTableType(t)
+	if next(t) == nil then return "Empty" end
+	local isArray = true
+	local isDictionary = true
+	for k, _ in next, t do
+		if typeof(k) == "number" and k%1 == 0 and k > 0 then
+			isDictionary = false
+		else
+			isArray = false
+		end
+	end
+	if isArray then
+		return "Array"
+	elseif isDictionary then
+		return "Dictionary"
+	else
+		return "MixedTable"
+	end
+end
+
+
+--[[
+	This is the WORST.
+	I have to do this to prevent extended classes being labeled as `*error-type*`. 
+	This is what allows autocomplete to function with classes.
+	Feel free to add more inheritance levels if it is TRULY NEEDED, but 4 should be enough for most projects.
+]]
+
+--[[
+	Abstract type for created objects. Can be used as a function return value for functions that return classes.
+	where T is type of initialization table
+]]
+export type Object<T> = T 
+& {
+	new: () -> T;
+	extend: <I>(T,I) -> InheritedObject<T & I, T>
+}
+--[[
+	Abstract type for objects that inherit from other objects. Can be used as a function return value for functions that return inherited classes.
+	where T is type of initialization tablev and I is inherited object type
+]]
+export type InheritedObject<T,I> = T 
+& {
+	new: () -> T;
+	extend: <N>(T,N) -> InheritedObject2<T & N,T>;
+	super: I;
+}
+
+type InheritedObject2<T,I> = T 
+& {
+	new: () -> T;
+	extend: <N>(T,N) -> InheritedObject3<T & N,T>;
+	super: I;
+}
+
+type InheritedObject3<T,I> = T 
+& {
+	new: () -> T;
+	extend: <N>(T,N) -> InheritedObject4<T & N,T>;
+	super: I;
+}
+
+type InheritedObject4<T,I> = T 
+& {
+	new: () -> T;
+	extend: <N>(T,N) -> unknown; -- will not work with autocomplete and will no longer be type-safe
+	super: I;
+}
+--[[
+	add more inheritance levels here if you genuinely need it
+	also add them to the unioned return type of mod.extend()
+]] 
+
+--[[
+	Extend the current object into a new object with the given table. Must be a dictionary.
+	Must be called with a :, i.e object:extend {} instead of object.extend {}
+]] 
+function ezobj.extend<I,T>(object: I, classtbl: T): InheritedObject<T,I> | InheritedObject2<T,I> | InheritedObject3<T,I> | InheritedObject4<T,I>
+	assert(typeof(classtbl) == "table", "Objects must be initialized with a dictionary.")
+	local t = getTableType(object)
+	if t ~= "Dictionary" and t ~= "Empty" then
+		return error("Expected Dictionary or Empty when initializing Object, got "..t)
+	end
+	for k,v in pairs(object) do
+		if classtbl[k] == nil then
+			classtbl[k] = v
+		end
+	end
+	local new_obj_metatable = {__index = classtbl}
+	classtbl.new = function(tbl: T?): Object<T>
+		local obj = setmetatable({},new_obj_metatable)
+		for i,v in pairs(classtbl) do
+			rawset(obj,i,v)
+		end
+		if tbl then
+			for k,v in pairs(tbl) do
+				rawset(obj,k,v)
+			end
+		end
+		return obj :: Object<T>
+	end
+	classtbl.super = object
+
+	return setmetatable(classtbl,{__index = ezobj})
+end
+
+-- Initialize an object with the given table. Must be a dictionary.
+function ezobj:__call<T>(object: T): Object<T>
+	-- where T: {[any]: any}
+	assert(typeof(object) == "table", "Objects must be initialized with a dictionary.")
+	local t = getTableType(object)
+	if t ~= "Dictionary" and t ~= "Empty" then
+		return error("Expected Dictionary or Empty when initializing Object, got "..t)
+	end
+
+	local new_obj_metatable = {__index = object}
+	object.new = function(tbl: T?): Object<T>
+		local obj = setmetatable({},new_obj_metatable)
+		for i,v in pairs(object) do
+			rawset(obj,i,v)
+		end
+		if tbl then
+			for k,v in pairs(tbl) do
+				rawset(obj,k,v)
+			end
+		end
+		return obj :: Object<T>
+	end
+
+	return setmetatable(object,{__index = ezobj})
+end
+
+
+
+return setmetatable(ezobj, ezobj)
